bbfd6ee933f987287fe49687ffd402c980999908
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2023-12-05 16:34:22 +0100

    Add coments

--- scripts/app.js
+++ scripts/app.js
@@ -1,55 +1,61 @@
 
 
 
-const filterButtons = document.querySelectorAll('.filter-button');
-const listItems = document.querySelectorAll('.list-item');
-const listItemsContainer = document.querySelector('.list-items');
+     //Filtering  image with price, style, type and format //
 
-let lastClickedFilterBy = null;
-let lastClickedFilterValue = null;
+	 //Creating variable and select elements//
 
-const filterList = (event) => {
-const currentButton = event.currentTarget;
+    const filterButtons = document.querySelectorAll('.filter-button');
+    const imageItems = document.querySelectorAll('.image-item');
+    const imageContainer = document.querySelector('.image-container');
 
-let buttonFilterBy = currentButton.dataset.filterBy;
-let buttonFilterValue = currentButton.dataset.filter;
 
-	if (buttonFilterValue === lastClickedFilterValue) {
-		buttonFilterValue = '*'
+    
+	let clikedBy = null;
+    let clickedValue= null;
+
+   
+	//Create a function //
+    //The function is called when button is clicked.//
+    const filterList = (event) => {              
+    const currentButton = event.currentTarget;
+    let currentButtonFilterBy = currentButton.dataset.filterBy;
+	let currentButtonFilterValue = currentButton.dataset.filter;
+
+	if (currentButtonFilterValue === clickedValue) {
+		currentButtonFilterValue = '*'
 	}
+    clikedBy = currentButtonFilterBy;
 
-	lastClickedFilterBy = buttonFilterBy;
-	lastClickedFilterValue = buttonFilterValue;
-
-	const filteredItems = [...listItems].filter(item => {
-		if (lastClickedFilterValue === '*') {
+	
+	//Creates an array that match the filter criteria.//
+    const filteredItems = [...imageItems].filter(item => {       
+		if (clickedValue === '*') {
 			return true;
 		} else {
 		
-			return item.dataset[buttonFilterBy] === buttonFilterValue;
+			return item.dataset[currentButtonFilterBy] === currentButtonFilterValue;
 		}
 	});
 
-
-	listItemsContainer.innerHTML = '';
-	
-
+	//Clears the content of imageContainer and  then add the filtered image.//
+	imageContainer.innerText = '';
 	filteredItems.forEach(item => {
-		listItemsContainer.appendChild(item);
+		imageContainer.appendChild(item);
 	});
 
-	
+	//Adding color to the button when its acctiv//
 	filterButtons.forEach(button => {
 		button.classList.remove('filter-button--active');
 	})
 
 
-	if (buttonFilterValue !== '*') {
+	if (currentButtonFilterValue !== '*') {
 		currentButton.classList.add('filter-button--active');
 	}
 }
 
-filterButtons.forEach(filterButton => {
+    filterButtons.forEach(filterButton => {
 	filterButton.addEventListener('click', filterList);
 });
 
@@ -57,22 +63,22 @@ filterButtons.forEach(filterButton => {
 
 
 
+  //This function is made so you can click on eash image-item and go to the component//
 
+     const overviewButtons = document.querySelectorAll('.image-item');
+     const components = document.querySelectorAll('.component');
+     const removeBackgroundItem = document.querySelector(".container");
+     const addButtonStyling = document.querySelector(".styling"); 
 
-
-const overviewButtons = document.querySelectorAll('.list-item');
-const components = document.querySelectorAll('.component');
-const removeBackgroundItem = document.querySelector(".container");
-const addButtonStyling = document.querySelector(".styling"); 
-
- overviewButtons.forEach(overviewButton => {
- const displayComponent = (event) => {
- const button = event.currentTarget;
- const componentToShow = button.dataset.componentToShow;
-
-
- removeBackgroundItem.style.display = "none";
- addButtonStyling.style.visibility = "visible";
+     
+	 
+	  overviewButtons.forEach(overviewButton => {
+      const displayComponent = (event) => {  // Get the current button that triggered the event//
+	  const button = event.currentTarget;            
+      const componentToShow = button.dataset.componentToShow; // Get the value of the 'data-component-to-show' attribute from the button//
+      removeBackgroundItem.style.display = "none";
+	  addButtonStyling.style.visibility = "visible";
+     
     
 
      components.forEach(component => {
@@ -91,9 +97,8 @@ const addButtonStyling = document.querySelector(".styling");
 
 
      
-	 
 
-	
+
 
 	function createTabsAndGallery(containerId,imageUrls) {
 		const tabsContainer = document.getElementById(`tabs${containerId}`);
@@ -104,8 +109,9 @@ const addButtonStyling = document.querySelector(".styling");
 		imageUrls.forEach((imageUrl, innerText) => {
 		
 		  const tab = document.createElement('div');
+		
 		  tab.classList.add('tab');
-		  tab.textContent = `Tab ${innerText + 1}`;
+		  tab.textContent = `Frame ${innerText + 1}`;
 		  tab.addEventListener('click', () => showImage(containerId, innerText));
 		  tabsContainer.appendChild(tab);
 
@@ -133,25 +139,32 @@ const addButtonStyling = document.querySelector(".styling");
 	  }
 	
 	  createTabsAndGallery(1, [
-		'assets/frame1.png',
 		'assets/frame2.png',
+		'assets/frame1.png',
 	
 	  ]);
 	
 
 
-	
+
 
 	
+
+
 	
+
+
+
+
+
+
 
 	  function backgroundChange(color) {
-		let colorarray = ["#DFE0B3", "#CDDBC6", "#E2DED4" ];
 		document.body.style.background = colorarray[color];
 	  }
-	  
+	 
 	  let colorarray = ["#DFE0B3", "#CDDBC6","#E2DED4"];
-	  let colorbox = document.getElementById("colorbox");
+	  let colorbox = document.querySelector("#colorbox");
 	  
 	  colorarray.forEach(function (color, innerText) {
 		let span = document.createElement("span");
